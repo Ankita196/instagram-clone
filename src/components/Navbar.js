b84ddef41ce1cff7f74d6812b1e5f488ec3e06b9
Ankita196 <muskanmakde1999@gmail.com>
2021-10-17 09:44:32 +0530

    2 files modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -70,215 +70,7 @@ marginTop:10
 
 export default function Navbar() {
   const classes = useStyles();
-  const [profile, setProfile] = useState([
-    {
-      id: 1,
-     
-     username: "its_ankita",
-     name:"Ankita Makade",
-    img:
-        "https://i.scdn.co/image/ab67616d0000b27354e544672baa16145d67612b",
-     followers:[
-      {
-        id: 1,
-       
-      username: "sapana",
-      location:"nagpur",
-      profile:
-          "https://i.scdn.co/image/ab67616d0000b27354e544672baa16145d67612b",
-        posts:"https://a10.gaanacdn.com/gn_img/albums/kGxbnw0Ky4/Gxbn8dG03y/size_xxl.jpg",
-        story:"https://a10.gaanacdn.com/gn_img/albums/kGxbnw0Ky4/Gxbn8dG03y/size_xxl.jpg"
-  
-      
-      },
-      {
-        id: 2,
-     
-      username: "raghvendra",
-      location:"hydrabad",
-      profile:
-          "https://a10.gaanacdn.com/gn_img/albums/kGxbnw0Ky4/Gxbn8dG03y/size_xxl.jpg",
-          posts:
-          "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg",
-       story:"https://i.scdn.co/image/ab67616d0000b27354e544672baa16145d67612b"
-      },
-      {
-        id: 3,
-      
-      username: "ankita",
-      name:"Ankita Makade",
-      location:"Nagpur",
-      profile:
-          "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg",
-      posts:"https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg",
-      story:"https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg"
-      },
-      {
-        id: 4,
-       
-      username: "tushar",
-      name:"Tushar Dighe",
-      location:"Nashik",
-      profile:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-          posts:"https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg",
-          story:"https://i.scdn.co/image/ab67616d0000b27354e544672baa16145d67612b"
-      },
-      {
-        id: 5,
-     
-      username: "milind",
-      name:"Milind Waghmare",
-      location:"nagpur",
-      profile:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-         posts:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-          story:"https://i.scdn.co/image/ab67616d0000b27354e544672baa16145d67612b"
-      },
-      {
-        id: 6,
-       
-      username: "yogesh",
-      name:"Yogesh kumar",
-      location:"Delhi",
-      profile:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-          posts:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-          story:"https://i.scdn.co/image/ab67616d0000b27354e544672baa16145d67612b"
-      },
-      
-      {
-        id: 7,
-     
-      username: "lalit",
-      name:"lalit Prajapati",
-      location:"nagpur",
-      profile:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-          posts:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-          story:"https://i.scdn.co/image/ab67616d0000b27354e544672baa16145d67612b"
-      },
-      {
-        id: 8,
-     
-      username: "neha",
-      name:"Neha Sontake",
-      location:"Pune",
-      profile:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-          posts:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-          story:"https://i.scdn.co/image/ab67616d0000b27354e544672baa16145d67612b"
-      }
-    
-    ],
-
-     
-     following:[
-      {
-        id: 1,
-       
-      username: "sapana",
-      location:"nagpur",
-      profile:
-          "https://i.scdn.co/image/ab67616d0000b27354e544672baa16145d67612b",
-        posts:"https://a10.gaanacdn.com/gn_img/albums/kGxbnw0Ky4/Gxbn8dG03y/size_xxl.jpg"
-  
-      
-      },
-      {
-        id: 2,
-     
-      username: "raghvendra",
-      location:"hydrabad",
-      profile:
-          "https://a10.gaanacdn.com/gn_img/albums/kGxbnw0Ky4/Gxbn8dG03y/size_xxl.jpg",
-          posts:
-          "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg",
-       
-      },
-      {
-        id: 3,
-      
-      username: "ankita",
-      name:"Ankita Makade",
-      location:"Nagpur",
-      profile:
-          "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg",
-      posts:"https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg",
-
-      },
-      {
-        id: 4,
-       
-      username: "tushar",
-      name:"Tushar Dighe",
-      location:"Nashik",
-      profile:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-          posts:"https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg",
-        
-      },
-      {
-        id: 5,
-     
-      username: "milind",
-      name:"Milind Waghmare",
-      location:"nagpur",
-      profile:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-         posts:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-       
-      },
-      {
-        id: 4,
-       
-      username: "yogesh",
-      name:"Yogesh kumar",
-      location:"Delhi",
-      profile:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-          posts:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-        
-      },
-      
-      {
-        id: 5,
-     
-      username: "lalit",
-      name:"lalit Prajapati",
-      location:"nagpur",
-      profile:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-          posts:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-      },
-      {
-        id: 6,
-     
-      username: "neha",
-      name:"Neha Sontake",
-      location:"Pune",
-      profile:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-          posts:
-          "https://images.hungama.com/c/1/9e3/fdd/53988130/53988130_300x300.jpg",
-      }
-    
-    ],
-
-     posts: [ "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg", "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg", "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg", "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg", "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg", "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg", "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg", "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg"] ,
-    
-   
-    igtv:[ "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg", "https://m.media-amazon.com/images/I/81hQLmFiFNL._SS500_.jpg",]
-    }
-  ])
-
+ 
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = (event) => {
@@ -289,8 +81,10 @@ export default function Navbar() {
     setAnchorEl(null);
   };
   return (
+    <>
+ 
     <div className={classes.grow}>
-      <div position="static" className={classes.appbar}>
+      <div position="fixed" className={classes.appbar}>
         <div className={classes.app}>
           <div>
             <Typography className={classes.title} variant="h6" noWrap>
@@ -350,5 +144,6 @@ export default function Navbar() {
       </div>
      
     </div>
+    </>
   );
 }
--- src/components/myposts.js
+++ src/components/myposts.js
@@ -3,6 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import ImageList from '@material-ui/core/ImageList';
 import ImageListItem from '@material-ui/core/ImageListItem';
 import {Link} from "react-router-dom"
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,7 +31,18 @@ const useStyles = makeStyles((theme) => ({
     marginLeft:900,
     width:200,
     height:30
-  }
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
   
 }));
 
@@ -35,9 +50,62 @@ const useStyles = makeStyles((theme) => ({
 export default function Myposts(props) {
   const classes = useStyles();
   const posts=props.posts.posts
+const [post,setPost]=useState("")
+
+  const onSelectFile = (e) => {
+    e.preventDefault();
+    if (e.target.files && e.target.files.length > 0) {
+      if (e.target.files.length > 0) {
+        setPost(URL.createObjectURL(e.target.files[0]));
+      }
+    }
+  };
+  const onsubmit = (e) => {
+    e.preventDefault();
+    setPost([
+      ...posts,
+
+      {
+        id: new Date().getTime().toString(),
+       
+        post: albumCover,
+      },
+    ]);
+   
+    setPost("");
+  };
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <>
-    <button className={classes.button}>upload</button> 
+    <div>
+    <button className={classes.button} onClick={handleOpen}>upload</button> <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">react-transition-group animates me.</p>
+          </div>
+        </Fade>
+      </Modal></div>
     <div className={classes.root}>
   
       <ImageList rowHeight={160} className={classes.imageList} cols={2}>
